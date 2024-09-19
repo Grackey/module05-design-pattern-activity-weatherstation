0ac54a289a4f9bf97e7b13779dc51abfbe0753d7
Allen Xu <purplebears321@gmail.com>
2024-09-19 14:06:51 -0400

    current conditions interface implementation

--- module05-design-pattern-activity-weatherstation/src/CurrentConditionsDisplay.ts
+++ module05-design-pattern-activity-weatherstation/src/CurrentConditionsDisplay.ts
@@ -1,8 +1,14 @@
 import WeatherData from './WeatherData';
+import { IWeatherDataObserver } from './WeatherDataObserver';
 
-export default class CurrentConditionsDisplay {
+export default class CurrentConditionsDisplay implements IWeatherDataObserver{
+
   static displayCurrentConditions(weatherData: WeatherData): void {
     // eslint-disable-next-line
+    console.log('Current conditions: %fF degrees and %f% humidity', weatherData.temperature, weatherData.humidity);
+  }
+
+  update(weatherData: WeatherData): void {
     console.log('Current conditions: %fF degrees and %f% humidity', weatherData.temperature, weatherData.humidity);
   }
 }
